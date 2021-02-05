89d9ac034e7fbcd2d6be07f26713df83da44514a
solutionman <solutionman@inbox.ru>
2021-02-05 12:25:31 +0700

    help

--- Files.cpp
+++ Files.cpp
@@ -10,6 +10,25 @@ int main(int argc, char **argv) {
     string fileName;
     if (argc > 1) {
         fileName = argv[1];
+        if(fileName.compare("-h")==0 || fileName.compare("--h")==0 || fileName.compare("-help")==0 || fileName.compare("--help")==0){
+            cout << "if file in current folder just type name of file with extension:\n"
+                    "./out\n"
+                    "test.txt\n"
+                    "\n"
+                    "out.exe\n"
+                    "test.txt\n"
+                    "\n"
+                    "if file in another directory, then name of file and path to file:\n"
+                    "./out\n"
+                    "write the name of the file: /home/dima/Documents/test.txt\n"
+                    "\n"
+                    "program can take optional arguments: nameOfFile and r\n"
+                    "for example this command will create or open file test.txt\n"
+                    "./out test.txt\n"
+                    "and this command will create and replace current file (if it already exists) test.txt\n"
+                    "./out test.txt r" << endl;
+            return 0;
+        }
     } else {
         cout << "write the name of the file: ";
         getline(cin, fileName);
